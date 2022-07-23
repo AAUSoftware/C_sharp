908b8482dff812fd71c390f4f32f2cb282d27d6d
Lyallvaldr <adi-01@live.dk>
2022-07-23 14:28:42 +0200

    Experiment1 update

--- Unity_3D/Experiment1/Assets/PrintToConsole.cs
+++ Unity_3D/Experiment1/Assets/PrintToConsole.cs
@@ -7,7 +7,7 @@ public class PrintToConsole : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+      Debug.Log("Print game start");  
     }
 
     // Update is called once per frame
